7a014e0d51c0966d068c40d7f1001126b9f1848f
Fernando Luis Dunaiski Bruginski <fernandodunaiski@gmail.com>
2022-01-26 11:06:40 -0300

    dia 3 concluído com sucesso 🥳

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+import { useRouter } from 'next/router';
+import {useState} from 'react';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-}
+
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -60,15 +35,15 @@ function Title(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'ferldb';
+    
+    const [username, setUsername] = useState('ferldb');
+    const router = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
-                    
                     backgroundImage: 'url(https://cdn.wccftech.com/wp-content/uploads/2020/08/TeamFight-Tactics-Fates-Keyart-scaled.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
@@ -91,6 +66,10 @@ export default function PaginaInicial() {
                     {/* Form */}
                     <Box
                         as="form"
+                        onSubmit={(infosDoEvento) => {
+                            infosDoEvento.preventDefault();
+                            router.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,6 +81,8 @@ export default function PaginaInicial() {
                         </Text>
 
                         <TextField
+                            value={username}
+                            onChange={(event)=>setUsername(event.target.value)}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
